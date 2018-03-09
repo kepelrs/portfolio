cb6c90d4b93824ae26d016c05430a33d7becd1ea
Davi <davivbarreto@gmail.com>
2018-03-09 08:55:30 +0200

    making the page personal

--- js/slides.js
+++ js/slides.js
@@ -1,4 +1,4 @@
-let featuredSlides = ["https://images.unsplash.com/photo-1452421822248-d4c2b47f0c81?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=b1a48d921059923e88ec28de5a615154&auto=format&fit=crop&w=1567&q=80"];
+let featuredSlides = [];
 let projectImgs = $(".featured-work img");
 let mainImage = $(".main-image");
 let counter = 1;
@@ -10,6 +10,5 @@ for(let i of projectImgs) {
 window.setInterval(function() {
     let targetSlide = "url(" + featuredSlides[counter] + ")";
     mainImage.css("background-image", targetSlide)
-    counter += 1;
-    counter = counter % featuredSlides.length;
+    counter = (counter + 1) % featuredSlides.length;
 }, 5000);
